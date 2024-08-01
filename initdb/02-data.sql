b38b4221e09bd2931d04ced0147b98dd39f8a241
Yadi Mulyadi <yadi.mulyadi86@gmail.com>
2024-08-01 10:45:45 +0700

    Deleted unnecessary files

--- initdb/02-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO transactions (client_code, report_code, transaction_date, transaction_time, status, nominal_tagihan, admin_fee, nominal_transaksi) VALUES
-('JLN', 'ACT-01', '2023-01-01', '08:00:00', 'SUCCESS', 100000, 5000, 105000),
-('JLN', 'ACT-01', '2023-01-01', '09:00:00', 'SUCCESS', 150000, 7500, 157500),
-('OTHER', 'ACT-02', '2023-01-01', '10:00:00', 'SUCCESS', 200000, 10000, 210000);
